fc1bf6117bf343cf3a6fe5a003eb7746a785c864
Thomas Wiringa <t.wiringa@youwe.nl>
2019-07-29 13:21:32 +0200

    Remove traefik label

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ docker run -d -p 127.0.0.1:53:53/tcp -p 127.0.0.1:53:53/udp \
     --cap-add=NET_ADMIN \
     -v /var/run/docker.sock:/tmp/docker.sock:ro \
     --name=dnsmasq \
-    --label="traefik.enable=false" \
+    --restart=always \
     lunamoonfang/dns-gen
